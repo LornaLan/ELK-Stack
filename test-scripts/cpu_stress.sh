667b156109e4ce45c921b466392c8e11906cebf7
LornaLan <lornalan@protonmail.com>
2021-09-25 18:31:47 -0400

    working remote stress test script

--- test-scripts/cpu_stress.sh
+++ test-scripts/cpu_stress.sh
@@ -8,11 +8,12 @@ sudo apt-get -y upgrade
 sudo apt-get install stress
 
 # Run the stress program and see Kibana update
-stress --cpu 1
+# Make sure this runs in the background to allow the 
+# later commands to be executed
+stress --cpu 1 & 
 
 # wait for 2 minutes before killing the process
 sleep 120
 
 # find the stress program and kill it
-prog_id=$(ps -a | grep stress | awk -F" " 'print $1}')
-kill $prog_id
+kill $(ps -a | grep stress | awk -F" " '{print $1}')
